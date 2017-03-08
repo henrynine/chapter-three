ed9b652c805d1383dcb244cb919ad324b8818b93
Henry Middleton <h78900@gmail.com>
2017-03-08 09:05:23 -0500

    Fix another typo in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = "sleepy-basin-95298.herokuapp.com"
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smpt_settings = {
+  ActionMailer::Base.smtp_settings = {
     :address            => 'smtp.sendgrid.net',
     :port               => '587',
     :authentication     => :plain,
